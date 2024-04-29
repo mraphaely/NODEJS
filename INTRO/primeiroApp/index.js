3098fd500137bce6f5769ae166faeae9fc20380d
Maryana Raphaely <138811691+mraphaely@users.noreply.github.com>
2024-04-29 15:23:43 -0300

    Delete INTRO/primeiroApp directory

--- INTRO/primeiroApp/index.js
+++
@@ -1,8 +0,0 @@
-//teminal integrado cmd -> node index.js
-const fs = require('node:fs')
-fs.readFile('teste.txt', 'utf-8', (err, data) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(data)
-})
