1e80bf5b9a01c3c9186b54bca82ec656a46717ce
Maryana Raphaely <138811691+mraphaely@users.noreply.github.com>
2024-04-29 15:24:39 -0300

    Add files via upload

---
+++ FUNDAMENTOS/abstracao_input/media.js
@@ -0,0 +1,5 @@
+const media = (arg1, arg2) => {
+   return (arg1 + arg2) / 2
+}
+
+export default media;
---
+++ FUNDAMENTOS/moduloInterno/index2.mjs
@@ -0,0 +1,5 @@
+import {soma} from "./meu_modulo2.mjs"
+
+const somar = soma;
+
+somar(1, 3);
