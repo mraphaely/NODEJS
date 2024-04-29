0be2ac5dd1441d4de78f1ed5b408f4c1a8f01b31
Maryana Raphaely <138811691+mraphaely@users.noreply.github.com>
2024-04-29 15:16:00 -0300

    Delete core_module directory

--- core_module/OS/index.js
+++
@@ -1,7 +0,0 @@
-const os = require('node:os');
-
-console.log(os.cpus());//quantos computares estão rodando na máquina
-console.log(os.cpus()[0].model);//computador especifico
-console.log(os.freemem());//Quantidade de memória disponivel em em bites
-console.log(os.homedir());//verifica qual diretorio está rodando a aplicação
-console.log(os.type());//vê o sistema operacional da máquina
