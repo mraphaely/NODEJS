0ad20c7f911f4ba88326dc5d8638e9e014f94334
Maryana Raphaely <138811691+mraphaely@users.noreply.github.com>
2024-05-24 18:32:37 -0300

    Add files via upload

--- API/2_HTTP_FS/server.js
+++ API/2_HTTP_FS/server.js
@@ -39,19 +39,53 @@ const server = http.createServer((request, response)=>{
                     return;
                 }
                 const livros = JSON.parse(data)
-                novoLivro.id = livros.lenght + 1
+                novoLivro.id = livros.length + 1
                 livros.push(novoLivro)
 
-                fs.writeFile('livros.json', JSON.stringify(livros))//parou aqui
-
-                console.log(livros)
-                return response.end();
+                fs.writeFile('livros.json', JSON.stringify(livros, null, 2),(err)=>{
+                    if (err) {
+                        response.writeHead(500,{"Content-Type" : "application/json"});
+                        response.end(JSON.stringify({message: 'Arquivo não encontrado'}));
+                        return;
+                    }
+                    response.writeHead(201, {"Content-Type" : "application/json"});
+                    response.end(JSON.stringify(novoLivro));
+                });    
             });
         });
-     }else{
+
+    } else if(false){
+
+    } else if(false){
+
+    } else if(method === 'GET' && url.startsWith('/livros/')){
+        const id = parseInt(url.split('/')[2]);
+        console.log(id)
+        fs.readFile('livros.json', 'utf8', (err, data)=>{
+            if(err){
+                response.writeHead(500, {'Content-Type': 'application/json'})
+                response.end(JSON.stringify({ message: 'Não foi possível acessar o arquivo'}))
+                return; 
+                }
+                const livros = JSON.parse(data)
+                
+                const encontrarLivro = livros.find((livro) => livro.id === id)
+            // console.log(encontrarLivro) //* V trás o livro | F - undefined
+            if(!encontrarLivro){
+                response.writeHead(400, {"Content-Type" : "application/json"});
+                response.end(JSON.stringify({message: "Livro não encontrado"}));
+                return;
+            }
+
+            // console.log(livros)
+            response.writeHead(200, {"Content-Type" : "application/json"})
+            response.end(JSON.stringify(encontrarLivro))
+
+        });
+    } else{
         response.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})
         response.end(JSON.stringify({ message: 'Página não encontrada'}))
-     }
+    }
 
 });
 
